2cfc9ea6acc1ddf186754baa6bebf26c0e995aa8
atemmel <temmeladam@gmail.com>
2023-03-03 00:11:51 +0100

    Mer

--- build.zig
+++ build.zig
@@ -1,18 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     b.addModule(.{
         .name = "zig-package-example",
         .source_file = std.Build.FileSource.relative("src/main.zig"),
     });
 
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
     const exe_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
 }
